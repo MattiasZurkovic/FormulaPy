c32de57d2422bbdf038df5dfa41eee728d272e84
MattiasZurkovic <mattias@zurkovic.com>
2015-08-13 18:33:47 +0200

    Add pythagorus formula

--- formula.py
+++ formula.py
@@ -1,1 +1,13 @@
-x = 'HELLO'
+import math
+
+def pythag(a, b, c):
+    if c == 'x':
+        return math.sqrt((a * a) + (b * b))
+    if a == 'x':
+        return math.sqrt((c * c) - (b * b))
+    if b == 'x':
+        return math.sqrt((c * c) - (a * a))
+
+
+
+print(pythag('x', -3, -5))
